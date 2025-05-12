c95f2731cbabecd6e334a6fcf1cc6a718e014a98
jamesoniensen <161636071+jamesoniensen@users.noreply.github.com>
2025-05-12 12:32:31 -0300

    Create layout.tsx
    Create layout.tsx

---
+++ ideal-site/layout.tsx
@@ -0,0 +1,12 @@
+// ideal-site/app/layout.tsx
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
