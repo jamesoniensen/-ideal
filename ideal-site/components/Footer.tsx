8b70ed535d0a62567de45963f80c074285d7e36b
jamesoniensen <161636071+jamesoniensen@users.noreply.github.com>
2025-05-14 17:26:49 -0300

    Update Footer.tsx

--- ideal-site/components/Footer.tsx
+++ ideal-site/components/Footer.tsx
@@ -1,4 +1,4 @@
 // ideal-site/components/Footer.tsx
 export default function Footer() {
-  return <footer>Footer Placeholder</footer>;
+  return <footer className="text-gold p-4 text-center">Footer Placeholder - Informações de Contato e Copyright Aqui</footer>;
 }
