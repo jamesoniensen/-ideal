d38bf3848fa3473479bab28284f64c42abc26e7b
jamesoniensen <161636071+jamesoniensen@users.noreply.github.com>
2025-05-14 17:26:19 -0300

    Update Navbar.tsx

--- ideal-site/components/Navbar.tsx
+++ ideal-site/components/Navbar.tsx
@@ -1,4 +1,4 @@
 // ideal-site/components/Navbar.tsx
 export default function Navbar() {
-  return <nav>Navbar Placeholder</nav>;
+  return <nav className="text-gold p-4">Navbar Placeholder - Em Breve Links e Logo Aqui!</nav>;
 }
