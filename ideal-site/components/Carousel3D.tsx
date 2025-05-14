2097fba74174c83be2d3fc7109480ad71955ab9c
jamesoniensen <161636071+jamesoniensen@users.noreply.github.com>
2025-05-14 17:27:23 -0300

    Update Carousel3D.tsx

--- ideal-site/components/Carousel3D.tsx
+++ ideal-site/components/Carousel3D.tsx
@@ -1,4 +1,4 @@
 // ideal-site/components/Carousel3D.tsx
 export default function Carousel3D() {
-  return <div>Carousel3D Placeholder</div>;
+  return <div className="text-gold p-4">Carousel3D Placeholder - Imagens e Interatividade em Breve!</div>;
 }
